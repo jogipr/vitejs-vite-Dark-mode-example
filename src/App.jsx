443fbc98374993c2e38b848ed0d703f2316d9799
jogipr <jogiprashant@gmail.com>
2024-07-30 17:39:00 +0530

    products page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import { useContext, useState, createContext, useEffect } from 'react';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import {Navigation } from './components/Navigation'
 import { ThemeProvider} from "./components/ThemeProvider";
 import { Switch} from "./components/Switch";
 import { Products} from "./components/Products";
+
 import './App.css';
 
 
 function App() {
   return (
     <>
+    <Router>
       <ThemeProvider>
         <header>
           <Navigation />
           <Switch />
         </header>
-        <Products/>
+        <Routes>
+          <Route path='/' element={ <Products/>}></Route>
+          <Route path='/products/:id' element={ <Products/>}></Route>
+        </Routes>
+       
       </ThemeProvider>
+      </Router>
     </>
   );
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,33 +1,44 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const Products = () => {
-
-  const [products,setProducts] = useState([]);
-
-  
-
-  useEffect(()=>{
-    const getProducts=async()=>{
-      const response=await fetch('https://dummyjson.com/products');
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate()
+  const [ skip ,setSkip]= useState(6);
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await fetch(`https://dummyjson.com/products?limit=6&skip=${skip}`);
       const data = await response.json();
       console.log(data)
-      setProducts(data.products);
-     }
+      setProducts(data.products?.slice(0,6));
+    }
     getProducts()
-  },[])
-    
-    return (
-      <div className="products-container">
-        {
-          products.map(product=>{
-            return (
-              <> 
-              <img src={product.thumbnail}/>
+  }, [skip])
+
+  const handleClick = (id) => {
+    navigate(`products/${id}`);
+  }
+  const handleNext=()=>{
+    setSkip(prev=> prev+6);
+  }
+
+  const handlePrev=()=>{
+    setSkip(prev=> prev-6);
+  }
+  return (
+    <div className="products-container">
+      {
+        products.map(product => {
+          return (
+            <div key={product.id} className='product' onClick={() => handleClick(product.id)}>
+              <img src={product.thumbnail} />
               <div>{product.title}</div>
-              </>
-            )
-          })
-        }
-      </div>
-    );
-  };
+            </div>
+          )
+        })
+      }
+      <button onClick={handlePrev}>Prev</button>
+      <button onClick={handleNext}>Next</button>
+    </div>
+  );
+};
